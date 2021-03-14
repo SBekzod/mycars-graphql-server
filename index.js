dd91c1ecaa0f244a605b0804531f0700b11a3999
Martin <martin@comets.kr>
2021-03-14 20:33:34 +0900

    feat: Apollo Server and Context

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const db = {
     ]
 }
 
-// create the schema
+// building SCHEMA
 const schema = gql(` 
 enum CarTypes {
    Sedan
@@ -56,39 +56,46 @@ enum CarTypes {
   }
 `)
 
-// create the resolvers
+// building RESOLVERS
 const resolvers = {
     Query: {
         carsByType: (parent, args, context, info) => {
-            return db.cars.filter(car => car.type === args.type)
+            return context.db.cars.filter(car => car.type === args.type)
         },
         carsById: (parent, args, context, info) => {
-            return db.cars.filter(car => car.id === args.id)[0]
-        }
-    },
-    Car: {
-        brand: (parent, args, context, info) => {
-            return db.cars.filter(car => car.brand === parent.brand)[0].brand
+            return context.db.cars.filter(car => car.id === args.id)[0]
         }
     },
     Mutation: {
-        insertCar: (_, { brand, color, doors, type }) => {
-            db.cars.push({
+        insertCar: (_, { brand, color, doors, type }, context) => {
+            context.db.cars.push({
                 id: Math.random().toString(),
                 brand: brand,
                 color: color,
                 doors: doors,
                 type: type
             })
-            return db.cars
+            return context.db.cars
         }
     }
 }
+
+const dbConnection = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            return resolve(db)
+        }, 100)
+    })
+}
+
+// BUILDING APOLLO SERVER AND LISTENING TO IT
 const server = new ApolloServer({
     typeDefs: schema,
-    resolvers
+    resolvers,
+    context: async () => {
+        return {db: await dbConnection()}
+    }
 })
-
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`)
 })
